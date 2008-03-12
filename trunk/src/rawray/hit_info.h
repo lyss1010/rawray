aae48230f91f86c4e7d99d4a6bc970931192a477
tamirh <tamirh@63339c38-7c47-0410-93cd-25e68fa0e233>
2008-03-12 07:34:00 +0000

    Regular barycentric triangle test working

--- trunk/src/rawray/hit_info.h
+++ trunk/src/rawray/hit_info.h
@@ -6,6 +6,7 @@
 #define RAWRAY_RAWRAY_HIT_INFO_H
 #include "stdafx.h"
 #include "math/vector3.h"
+#include "math/vector2.h"
 
 /////////////////////////////////////////////////////////////////////////////
 namespace rawray {
@@ -18,14 +19,17 @@ public:
     float distance;
     Vector3 point;
     Vector3 normal;
+    Vector2 texCoord;
+
     const Material* material; // TODO: Make this a reference and create a "blank" material to assign to it
 
     HitInfo(const HitInfo& hit) : distance(hit.distance), point(hit.point), normal(hit.normal), material(hit.material) { }
     explicit HitInfo(float distance = 0.0f, 
                      const Vector3& point = Vector3(),
                      const Vector3& normal = Vector3(0.0f, 1.0f, 0.0f),
+                     const Vector2& texCoord = Vector2(0.0f, 0.0f),
                      const Material* material = NULL) :
-        distance(distance), point(point), normal(normal), material(material) { }
+        distance(distance), point(point), normal(normal), texCoord(texCoord), material(material) { }
 
 }; // class HitInfo
 
--- trunk/src/rawray/triangle.cpp
+++ trunk/src/rawray/triangle.cpp
@@ -27,6 +27,9 @@ bool Triangle::Intersect(HitInfo& hit, const Ray& ray, float minDistance, float 
     case options::BARYCENTRIC:
         return Barycentric(hit, ray, minDistance, maxDistance);
 
+    case options::BARYCENTRIC_PROJECTION:
+        return BarycentricProjection(hit, ray, minDistance, maxDistance);
+
     case options::MOLLER:
         return Moller(hit, ray, minDistance, maxDistance);
 
@@ -39,43 +42,125 @@ bool Triangle::Intersect(HitInfo& hit, const Ray& ray, float minDistance, float 
 }
 
 bool Triangle::Barycentric(HitInfo& hit, const Ray& ray, float minDistance, float maxDistance) {
-    // See: http://www.devmaster.net/wiki/Ray-triangle_intersection
-	const Tuple3I indices = mesh_.GetVertexIndices()[ index_ ];
-    const Vector3& v0 = mesh_.GetVertices()[ indices.x ];
-    const Vector3& v1 = mesh_.GetVertices()[ indices.y ];
-    const Vector3& v2 = mesh_.GetVertices()[ indices.z ];
-	const Vector3& n = mesh_.GetNormals()[ index_ ];
+	const Tuple3I vertexIndices = mesh_.GetVertexIndices()[ index_ ];
+    const Vector3& v0 = mesh_.GetVertices()[ vertexIndices.x ];
+    const Vector3& v1 = mesh_.GetVertices()[ vertexIndices.y ];
+    const Vector3& v2 = mesh_.GetVertices()[ vertexIndices.z ];
 
-	// Compute distance from ray origin to triangle plane
-	register float divisor = ray.origin.Dot(n);
-	if( divisor == 0.0f ) return false;
-	hit.distance = -(ray.origin - v0).Dot(n) / divisor;
-	if( hit.distance < 0.0f ) return false;
-	
-	// Early rejection (behind us) failed, compute full barycentric coords
-	hit.point = (ray.origin + hit.distance * ray.direction);
-	const Vector3 p = hit.point - v0;
-	const Vector3 b = v1 - v0;
-	const Vector3 c = v2 - v0;
+    // Normal of the plane which the triangle is on (may not be equivilent to individual triangle normals)
+    const Vector3& e1 = v1 - v0;
+    const Vector3& e2 = v2 - v0;
+    const Vector3& n = math::Cross( e1, e2 ).Normalize();
 
-	divisor = b.x * c.x - b.x * c.y;
-	if( divisor == 0.0f ) return false;
-	const float u = (p.y * c.x - p.x * c.y) / divisor;
-	if( u < 0.0f ) return false;
+    // See if we are exactly lined up with the plane
+    register float denominator = math::Dot( ray.direction, n );
+    if( denominator == 0.0f ) return false;
 
-	divisor = c.y * b.x - c.x * b.y;
-	if( divisor == 0.0f ) return false;
-	const float v = (p.y * b.x - p.x * b.y) / divisor;
-	if( v < 0.0f ) return false;
+    // Compute the point on the plane where we intersect
+    const float distance = - math::Dot( ray.origin - v0, n ) / denominator;
+    if( distance < minDistance || distance > maxDistance ) return false;
+    const Vector3& h = (ray.direction * distance) + ray.origin;
 
-	if( u+v <= 1.0f )
-	{
-		hit.material = material_;
-		hit.normal = n;
-		return true;
-	}
-	
-	return false;
+    // Compute barycentric coords
+    // See: http://www.cimec.org.ar/twiki/pub/Cimec/ComputacionGrafica/triangle_intersection.pdf
+    denominator = math::Dot( math::Cross(e1,e2), n );
+    if( denominator == 0.0f ) return false;
+    
+    const float alpha = math::Dot( math::Cross( v2-v1, h-v1 ), n ) / denominator;
+    if( alpha < 0.0f ) return false;
+
+    const float beta = math::Dot( math::Cross( v0-v2, h-v2 ), n ) / denominator;
+    if( beta < 0.0f ) return false;
+    if( alpha+beta > 1.0f ) return false;
+
+    // Copy values we computed earlier for hit point and distance
+    hit.point = h;
+    hit.distance = distance;
+    hit.material = material_;
+
+    // Interpolations
+    const float gamma = 1 - alpha - beta;
+
+    const Tuple3I normalIndices = mesh_.GetNormalIndices()[ index_ ];
+	const Vector3& n0 = alpha * mesh_.GetNormals()[ normalIndices.x ];
+    const Vector3& n1 = beta * mesh_.GetNormals()[ normalIndices.y ];
+    const Vector3& n2 = gamma * mesh_.GetNormals()[ normalIndices.z ];
+
+    hit.normal.x = n0.x + n1.x + n2.x;
+    hit.normal.y = n0.y + n1.y + n2.y;
+    hit.normal.z = n0.z + n1.z + n2.z;
+
+    if( mesh_.GetTexCoordIndices() != NULL ) {
+        const Tuple3I texCoordIndices = mesh_.GetTexCoordIndices()[ index_ ];
+        const Vector2& t0 = alpha * mesh_.GetTexCoords()[ texCoordIndices.x ];
+        const Vector2& t1 = beta * mesh_.GetTexCoords()[ texCoordIndices.y ];
+        const Vector2& t2 = gamma * mesh_.GetTexCoords()[ texCoordIndices.z ];
+
+        hit.texCoord.x = t0.x + t1.x + t2.x;
+        hit.texCoord.y = t0.y + t1.y + t2.y;
+    } else
+        hit.texCoord = Vector2();
+
+    return true;
+}
+
+bool Triangle::BarycentricProjection(HitInfo& hit, const Ray& ray, float minDistance, float maxDistance) {
+	const Tuple3I vertexIndices = mesh_.GetVertexIndices()[ index_ ];
+    const Vector3& v0 = mesh_.GetVertices()[ vertexIndices.x ];
+    const Vector3& v1 = mesh_.GetVertices()[ vertexIndices.y ];
+    const Vector3& v2 = mesh_.GetVertices()[ vertexIndices.z ];
+
+    // Normal of the plane which the triangle is on (may not be equivilent to individual triangle normals)
+    const Vector3& c = v2 - v0;
+    const Vector3& b = v1 - v0;
+    const Vector3& n = math::Cross( c, b ).Normalize();
+
+    // See if we are exactly lined up with the plane
+    register float denominator = math::Dot( ray.direction, n );
+    if( denominator == 0.0f ) return false;
+
+    // Compute distance to the plane along the ray
+    const float distance = math::Dot( v0 - ray.origin, n ) / denominator;
+    if( distance < minDistance || distance > maxDistance ) return false;
+
+    // Optimized barycentric projection test
+    // See: http://www.mpi-inf.mpg.de/~wald/PhD/wald_phd.pdf
+
+    // Determine dominant axis
+    const float absNX = abs(n.x);
+    const float absNY = abs(n.y);
+    const float absNZ = abs(n.z);
+
+    uint8 axis; // 0=x, 1=y, 2=z
+    if( absNX > absNY )
+        axis = (absNX > absNZ) ? 0 : 2;
+    else
+        axis = (absNY > absNZ) ? 1 : 2;
+
+    const uint8 u = (axis+1)%3;
+    const uint8 v = (axis+2)%3;
+
+    // Compute the point on the plane where we intersect
+    const Vector3& h = (ray.direction * distance) + ray.origin;
+
+    denominator = b[u] * c[v] - b[v] * c[u];
+    if( denominator == 0.0f ) return false;
+    denominator = 1.0f / denominator;
+
+    const float beta = (b[u] * h[v] - b[v] * h[u]) * denominator;
+    if( beta < 0.0f ) return false;
+
+    const float gamma = (c[v] * h[v] - c[u] * h[v]) * denominator;
+    if( gamma < 0.0f ) return false;
+    if( beta+gamma > 1.0f ) return false;
+
+    // TODO: Compute better normal etc
+    hit.normal = n;
+    hit.distance = distance;
+    hit.material = material_;
+    hit.point = h;
+
+    return true;
 }
 
 bool Triangle::Moller(HitInfo& hit, const Ray& ray, float minDistance, float maxDistance) {

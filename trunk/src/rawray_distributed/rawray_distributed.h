0fb2cbd120c3a743a5191d74cf5ed609f04eb56f
tamirh <tamirh@63339c38-7c47-0410-93cd-25e68fa0e233>
2008-04-11 20:25:39 +0000

    distributed project not compiling even with just skeleton =(

---
+++ trunk/src/rawray_distributed/rawray_distributed.h
@@ -0,0 +1,13 @@
+/////////////////////////////////////////////////////////////////////////////
+// Distributed Console Entry Point : rawray_console.h
+//
+/////////////////////////////////////////////////////////////////////////////
+#ifndef RAWRAY_RAWRAY_DISTRIBUTED_RAWRAY_DISTRIBUTED_H
+#define RAWRAY_RAWRAY_DISTRIBUTED_RAWRAY_DISTRIBUTED_H
+#include "stdafx.h"
+
+
+bool ConnectToServer(uint16 port);
+bool ListenForTasks(uint16 port, int numSockets)
+
+#endif // RAWRAY_RAWRAY_DISTRIBUTED_RAWRAY_DISTRIBUTED_H
---
+++ trunk/src/rawray_distributed/stdafx.h
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////////////////////////
+// Precompiled Header : stdafx.h
+//
+// Common includes and definitions of rawray_distributed project
+/////////////////////////////////////////////////////////////////////////////
+#ifndef RAWRAY_RAWRAY_DISTRIBUTED_STDAFX_H
+#define RAWRAY_RAWRAY_DISTRIBUTED_STDAFX_H
+#include "base/global_defs.h"
+
+
+// Windows Includes
+#include <Windows.h>
+#include <Winsock2.h>
+
+
+// Memory leak detection
+#include <stdlib.h>
+#include <crtdbg.h>
+
+
+// C RunTime Header Files
+#include <iostream>
+#include <tchar.h>
+
+
+#endif // RAWRAY_RAWRAY_DISTRIBUTED_STDAFX_H

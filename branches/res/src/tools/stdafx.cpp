65b5c2de3a8b024e366caad1fba6c18f06c62a7c
tamirh <tamirh@63339c38-7c47-0410-93cd-25e68fa0e233>
2008-04-11 23:23:45 +0000

    Removed res branch because storing the large files puts us over googlecode's size limit

--- branches/res/src/tools/stdafx.cpp
+++
@@ -1,4 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// tools.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-#include "stdafx.h"

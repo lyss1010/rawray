16bab228323e19ca81b92dec8cf63f8924e93ec8
tamirh <tamirh@63339c38-7c47-0410-93cd-25e68fa0e233>
2008-05-02 05:21:40 +0000

    lol, it compiles now

--- trunk/src/rawray/instance.h
+++ trunk/src/rawray/instance.h
@@ -2,8 +2,8 @@
 // Class : bbox_aa.h
 //
 /////////////////////////////////////////////////////////////////////////////
-#ifndef RAWRAY_RAWRAY_BBOX_AA_H
-#define RAWRAY_RAWRAY_BBOX_AA_H
+#ifndef RAWRAY_RAWRAY_INSTANCE_H
+#define RAWRAY_RAWRAY_INSTANCE_H
 #include "stdafx.h"
 #include "object.h"
 
@@ -16,11 +16,11 @@ class DllExport Instance : public Object
 {
 public:
     Instance(Material* material) : Object(material) { }
-    Instance(Material* material, Object* parent) : Object(material) parent_(parent) { }
+    Instance(Material* material, Object* instancedObject) : Object(material), instancedObject_(instancedObject) { }
 
     // TODO: Take into account matrix rotations on min/max
-    virtual Vector3 GetMin() { return object_->GetMin(); }
-    virtual Vector3 GetMax() { return object_->GetMax(); }
+    virtual Vector3 GetMin() { return instancedObject_->GetMin(); }
+    virtual Vector3 GetMax() { return instancedObject_->GetMax(); }
 
     virtual ~Instance() { }
 
@@ -32,22 +32,14 @@ public:
     virtual void IntersectPack(HitPack& hitpack, float minDistance, float maxDistance);
 
 private:
-    BBoxAA(Material* material) : Object(material) { }
+    Object* instancedObject_;
 
-    BBoxAA(Material* material, const Vector3& min, const Vector3& max) : Object(material) {
-        bounds_[0] = min;
-        bounds_[1] = max;
-    }
+    DISALLOW_IMPLICIT_CONSTRUCTORS(Instance);
 
-    SSE_ALIGN Vector3 bounds_[2]; // { min, max }
-    std::vector<Object*> objects_;
-
-    DISALLOW_IMPLICIT_CONSTRUCTORS(BBoxAA);
-
-}; // class BBoxAA
+}; // class Instance
 
 } // namespace rawray
 /////////////////////////////////////////////////////////////////////////////
 
 
-#endif // RAWRAY_RAWRAY_BBOX_AA_H
+#endif // RAWRAY_RAWRAY_INSTANCE_H

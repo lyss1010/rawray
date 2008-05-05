7f0067a035ee7821f7bc710c5c4d45b9ccf14986
tamirh@gmail.com <tamirh@gmail.com@63339c38-7c47-0410-93cd-25e68fa0e233>
2008-05-05 21:50:45 +0000

    Changed from returning iterators to returning indicies in the bvh private functions

--- src/rawray/bvh.h
+++ src/rawray/bvh.h
@@ -16,19 +16,19 @@ class BBoxAA;
 
 struct BVHNode {
     bool isLeaf;
-
+	
     union {
         BBoxAA*  bbox;
         BVHNode* children; // [left, right]
     };
-
+	
     bool Intersect(HitInfo& hit, float minDistance, float maxDistance);
     void IntersectPack(HitPack& hitpack, float minDistance, float maxDistance);
     void BuildBVH( std::vector<BBoxAA*>& forrset );
 
 private:
-    int8 Split( std::vector<BBoxAA*>::iterator split, std::vector<BBoxAA*>* sorted );
-    std::vector<BBoxAA*>::iterator FindSplittingPlane( std::vector<BBoxAA*> sorted );
+    int8 Split( size_t& splitIndex, std::vector<BBoxAA*>* sorted );
+    size_t FindSplittingPlane( std::vector<BBoxAA*>& sorted );
     float Cost(float areaLeft, float areaRight);
 };
 
@@ -54,7 +54,6 @@ private:
     BVHNode root_;
     float ojb_cost, box_cost;
     std::vector<Object*>* objects_;
-    
     
     DISALLOW_COPY_CONSTRUCTORS(BVH);
 

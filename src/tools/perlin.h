819aabde27345089635ee813841f012e4cfb7718
tamirh <tamirh@63339c38-7c47-0410-93cd-25e68fa0e233>
2008-05-14 22:32:50 +0000

    Added Perlin class from site Added cornell box model

---
+++ src/tools/perlin.h
@@ -0,0 +1,31 @@
+/////////////////////////////////////////////////////////////////////////////
+// Class : perlin.h
+//
+/////////////////////////////////////////////////////////////////////////////
+#ifndef RAWRAY_TOOLS_PERLIN_H
+#define RAWRAY_TOOLS_PERLIN_H
+#include <math.h>
+
+/////////////////////////////////////////////////////////////////////////////
+namespace tools {
+
+
+// Based on Java reference implementation of:
+// Improved Noise - Copyright 2002 Ken Perlin.
+// See: http://mrl.nyu.edu/~perlin/noise/
+class Perlin {
+public:
+   static float Noise(float x, float y, float z);
+
+private:
+   static float Fade(float t);
+   static float Lerp(float t, float a, float b);
+   static float Grad(int hash, float x, float y, float z);
+
+   static const unsigned char p[512];
+}; // class Perlin
+
+} // namespace tools
+/////////////////////////////////////////////////////////////////////////////
+
+#endif // RAWRAY_TOOLS_PERLIN_H

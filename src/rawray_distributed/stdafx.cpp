bf7e85268846927dd7d912b1da44749f67d079e9
tamirh@gmail.com <tamirh@gmail.com@63339c38-7c47-0410-93cd-25e68fa0e233>
2008-05-14 22:06:31 +0000

    Getting rid of non-model files

--- src/rawray_distributed/stdafx.cpp
+++
@@ -1,4 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// rawray_distributed.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-#include "stdafx.h"
